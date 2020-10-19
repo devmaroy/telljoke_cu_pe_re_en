84e60f4c0ee8525d6b9671abf2d958e386c99c1a
Marek Matejovic <me@devmaroy.cz>
2020-10-19 20:07:36 +0200

    Update ContactPage.js

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -5,7 +5,7 @@ const ContactPage = () => (
         <div className="container">
             <h1 className="page__title">Contact</h1>
             <p>Author: Marek Matejovic</p>
-            <p>Web: <a href="https://devmaroy.com/">devmaroy.com</a></p>
+            <p>Web: <a href="https://marekmatejovic.com/">devmaroy.com</a></p>
             <p>For any questions feel free to contact me at <a href="mailto:hello@devmaroy.com">hello@devmaroy.com</a></p>
         </div>
     </div>
