08a0b939029063ac030a09c795a423112ce72023
Marek Matejovic <me@devmaroy.cz>
2020-10-19 20:07:48 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
     <div className="footer">
-        <p>Made with <span className="heart-icon">&#10084;</span> by <a href="https://devmaroy.com/">devmaroy</a></p>
+        <p>Made with <span className="heart-icon">&#10084;</span> by <a href="https://marekmatejovic.com/">devmaroy</a></p>
     </div>
 );
 
